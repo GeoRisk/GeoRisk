e9c0d745490f7592bc101ce25376fb4dca278a44
Martins Ewere <martinsewere@gmail.com>
2017-08-23 19:03:11 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -20,20 +20,21 @@ var sequelize = new Sequelize("georisk", "root", "password") {
 	}
 
 }
-module.exports = sequelize;
 
 // var connection = mysql.createConnection({		
- //   host: "127.0.0.1",		
- //   user: "root",		
- //   password: "",		
- //   database: "georisk"		
- // });		
- 		
- 		
- // connection.connect(function(err) {		
- //   if (err) {		
- //     console.error("error connecting: " + err.stack);		
- //     return;		
- //   }		
- //   console.log("connected as id " + connection.threadId);		
- // });
+//   host: "127.0.0.1",		
+//   user: "root",		
+//   password: "",		
+//   database: "georisk"		
+// });		
+
+
+// connection.connect(function(err) {		
+//   if (err) {		
+//     console.error("error connecting: " + err.stack);		
+//     return;		
+//   }		
+//   console.log("connected as id " + connection.threadId);		
+// });
+
+module.exports = sequelize;
